766e846f9ae7fb339a2835b60068b1ec4756bf1e
Lucas Dopke <dopkelucas@gmail.com>
2025-01-16 16:05:58 -0300

    primeiro commit

--- src/Moto.java
+++ src/Moto.java
@@ -1,4 +1,3 @@
-package PACKAGE_NAME;
+public class Moto extends Veiculo {
 
-public class Moto {
 }
--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Veiculo {
+    String marca;
+    String modelo;
+    int ano;
+
+    protected void exibirDetalhes(){
+
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+
+    }
 }
